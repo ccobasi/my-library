2be920a13ffd9f6802d7fee15d4d486b88dcb13a
Chukwuma Obasi <ccobasi8@gmail.com>
2021-06-22 23:13:37 +0100

    Add on click event on read

--- app.js
+++ app.js
@@ -20,15 +20,32 @@ function displayBook(newBook) {
     <td>${newBook.title}</td>
     <td>${newBook.author}</td>
     <td>${newBook.pages}</td>
-    <td><a href="#" class="btn btn-warning btn-sm ">Unread</a></td>
+    <td><a href="#" id="unread" class="btn btn-warning btn-sm unread">Unread</a></td>
     <td><a href="#" class="btn btn-danger btn-sm delete">Delete</a></td>`;
 
   list.appendChild(row);
 }
 
+const btnSuccess = document.createElement('td');
+btnSuccess.textContent = 'Read';
+btnSuccess.setAttribute('class', ' btn btn-success');
+
 document.querySelector('#book-list').addEventListener('click', (e) => {
   if (e.target.classList.contains('delete')) {
     e.target.parentElement.parentElement.remove();
+  }
+});
+
+document.querySelector('#book-list').addEventListener('click', (e) => {
+  if (e.target.classList.contains('unread')) {
+    e.target.setAttribute('class', ' btn btn-success');
+    e.target.textContent = 'Read';
+  } else if (e.target.classList.contains('btn-success')) {
+    e.target.setAttribute('class', ' btn btn-warning');
+    e.target.textContent = 'Unread';
+  } else {
+    e.target.setAttribute('class', ' btn btn-success');
+    e.target.textContent = 'Read';
   }
 });
 
