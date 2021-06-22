6a7f7cbaf7ef346717f3285f4faecd122876228a
Chukwuma Obasi <ccobasi8@gmail.com>
2021-06-22 17:12:00 +0100

    Add display book function

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read = false) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(newBook) {
+  myLibrary.push(newBook);
 }
+
+function displayBook(newBook){
+    const list = document.querySelector('#book-list');
+    const row = document.createElement('tr');
+    row.innerHTML = `
+    <td>${Book.title}</td>
+    <td>${Book.author}</td>
+    <td>${Book.pages}</td>
+    <td><a href="#" class="btn btn-warning btn-sm ">Unread</a></td>
+    <td><a href="#" class="btn btn-danger btn-sm delete"><i class="fas fa-trash"></a></td>`;
+
+    list.appendChild(row);
+}
+    
